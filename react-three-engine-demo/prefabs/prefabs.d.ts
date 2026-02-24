143692180515d52bd369b781aa488ebcb6a85239
최지원 <66864266+chamchi0809@users.noreply.github.com>
2026-02-24 18:59:11 +0900

    stage prefabs folder

---
+++ react-three-engine-demo/prefabs/prefabs.d.ts
@@ -0,0 +1,9 @@
+import "react-three-engine";
+import * as THREE from "three/webgpu";
+
+declare module "react-three-engine" {
+  interface PrefabTypeRegistry {
+    "1234": Omit<import("three").Group, "children"> & { children: [THREE.Mesh, THREE.Mesh] };
+    "test-prefab": Omit<import("three").Group, "children"> & { children: [THREE.Mesh] };
+  }
+}
